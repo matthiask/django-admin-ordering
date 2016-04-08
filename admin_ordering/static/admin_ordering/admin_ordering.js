1ed63ecf86571ae9a99023d5f978b2b3112b2034
Matthias Kestenholz <mk@feinheit.ch>
2016-04-08 11:41:32 +0200

    Add support for stacked inlines

--- admin_ordering/static/admin_ordering/admin_ordering.js
+++ admin_ordering/static/admin_ordering/admin_ordering.js
@@ -17,5 +17,16 @@ django.jQuery(function($){
                 });
             }
         });
+    } else if (data.stacked) {
+        $('#' + data.prefix + '-group').sortable({
+            items: '>.inline-related',
+            update: function(event, ui) {
+                $('.dynamic-' + data.prefix).each(function(index) {
+                    var row = $(this);
+                    row.find('.field-' + data.field + ' input').val(10 * (index + 1));
+                    row.removeClass('row1 row2').addClass((index % 2) ? 'row2' : 'row1');
+                });
+            }
+        });
     }
 });
