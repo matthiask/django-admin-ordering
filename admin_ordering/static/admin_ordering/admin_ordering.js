48b800504f9ab8799da6a00ab6a583e392a87e91
Fabio Caccamo <fabio.caccamo@gmail.com>
2018-02-19 17:24:03 +0100

    Added ordering icon / hidden ordering field and improved ui.

--- admin_ordering/static/admin_ordering/admin_ordering.js
+++ admin_ordering/static/admin_ordering/admin_ordering.js
@@ -26,37 +26,110 @@ django.jQuery(function($){
         }
 
         if (data.tabular) {
-            $('#' + data.prefix + '-group tbody').sortable({
+
+            $sortable = $('#' + data.prefix + '-group tbody');
+            $sortable.sortable({
                 items: '>.has_original',
                 start: function(event, ui){
-                    ui.placeholder.height(ui.item.height());
+                    // hide body horizontal overflow while dragging row
+                    $('body').css('overflow-x', 'hidden');
+
+                    // set placeholder height equal to item height
+                    ui.placeholder.height(ui.item.outerHeight());
+
+                    // fix ui item appereance
+                    ui.item.css('height', ui.item.outerHeight());
                 },
                 update: function(event, ui) {
                     updateOrdering($('.dynamic-' + data.prefix));
+                },
+                stop: function(event, ui){
+                    // reset body horizontal overflow
+                    $('body').css('overflow-x', 'auto');
+
+                    ui.item.css('height', 'auto');
                 }
             });
+
+            // add custom css class to ordering field to customize it
+            $sortable.find('.field-' + data.field).addClass('admin-ordering-field');
+
+            // enforce row cells size while sorting rows
+            $sortable.find('>tr').each(function(){
+                $(this).mousedown(function(e){
+                    $(this).find('td, th').each(function(){
+                        $(this).css('width', $(this).width());
+                    });
+                }).mouseup(function(e){
+                    $(this).find('td, th').each(function(){
+                        $(this).css('width', 'auto');
+                    });
+                });
+            });
+
         } else if (data.stacked) {
-            $('#' + data.prefix + '-group').sortable({
+
+            $sortable = $('#' + data.prefix + '-group');
+            $sortable.sortable({
                 items: '>.has_original,>>.has_original',
                 start: function(event, ui){
-                    ui.placeholder.height(ui.item.height());
+                    // hide body horizontal overflow while dragging row
+                    $('body').css('overflow-x', 'hidden');
+
+                    // set placeholder height equal to item height
+                    ui.placeholder.height(ui.item.outerHeight());
                 },
                 update: function(event, ui) {
                     updateOrdering($('.dynamic-' + data.prefix));
+                },
+                stop: function(event, ui){
+                    // reset body horizontal overflow
+                    $('body').css('overflow-x', 'auto');
                 }
             });
-        } else {
-            var $tbody = $('#result_list tbody');
-            if (!$tbody.find('.field-' + data.field + ' input').length)
-                return;
 
-            $tbody.sortable({
+            // add custom css class to ordering field to customize it
+            $sortable.find('.field-' + data.field).addClass('admin-ordering-field');
+
+        } else {
+
+            var $sortable = $('#result_list tbody');
+            if (!$sortable.find('.field-' + data.field + ' input').length){
+                return;
+            }
+
+            $sortable.sortable({
+                // make the dragged row a little bit transparent
                 start: function(event, ui){
-                    ui.placeholder.height(ui.item.height());
+                    // hide body horizontal overflow while dragging row
+                    $('body').css('overflow-x', 'hidden');
+
+                    // set placeholder height equal to item height
+                    ui.placeholder.height(ui.item.outerHeight());
                 },
                 update: function(event, ui) {
-                    updateOrdering($tbody.find('tr'));
+                    updateOrdering($sortable.find('tr'));
+                },
+                stop: function(event, ui){
+                    // reset body horizontal overflow
+                    $('body').css('overflow-x', 'auto');
                 }
+            });
+
+            // add custom css class to ordering field to customize it
+            $sortable.find('.field-' + data.field).addClass('admin-ordering-field');
+
+            // enforce row cells size while sorting rows
+            $sortable.find('>tr').each(function(){
+                $(this).mousedown(function(e){
+                    $(this).find('td, th').each(function(){
+                        $(this).css('width', $(this).width());
+                    });
+                }).mouseup(function(e){
+                    $(this).find('td, th').each(function(){
+                        $(this).css('width', 'auto');
+                    });
+                });
             });
         }
     });
