a001c448f71c6ae2d29bc0c0cb8f95884a2d60a1
Matthias Kestenholz <mk@feinheit.ch>
2024-09-13 08:34:59 +0200

    Make the linter happy

--- tests/testapp/models.py
+++ tests/testapp/models.py
@@ -6,6 +6,9 @@ from admin_ordering.models import OrderableModel
 class Parent1(models.Model):
     title = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.title
+
 
 class Child1(models.Model):
     parent = models.ForeignKey(
@@ -13,9 +16,15 @@ class Child1(models.Model):
     )
     ordering = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.title
+
 
 class Parent2(models.Model):
     title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
 
 
 class Child2(models.Model):
@@ -24,19 +33,31 @@ class Child2(models.Model):
     )
     ordering = models.IntegerField(default=0)
 
+    def __str__(self):
+        return ""
+
 
 class Parent3(models.Model):
     title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
 
 
 class Child3(models.Model):
     parent = models.ForeignKey(Parent3, on_delete=models.CASCADE)
     ordering = models.IntegerField(default=0)
 
+    def __str__(self):
+        return ""
+
 
 class Parent4(models.Model):
     title = models.CharField(max_length=100)
     _orderaaaaa = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
 
 
 class Orderable(OrderableModel):
